1bfa164b105f8a2b96d316aaf2775a2c67d305df
Matt Ware <mattbware3@gmail.com>
2023-06-11 03:20:44 +0100

    Finished ShaderProgram::GetShaderTypeString

--- MatrixMentor/Source/Core/Rendering/Shaders/ShaderProgram.cpp
+++ MatrixMentor/Source/Core/Rendering/Shaders/ShaderProgram.cpp
@@ -104,6 +104,12 @@ const char* ShaderProgram::GetShaderTypeString(GLenum type)
 		return "Fragment Shader";
 	case GL_GEOMETRY_SHADER:
 		return "Geometry Shader";
+	case GL_TESS_CONTROL_SHADER:
+		return "Tessellation Control Shader";
+	case GL_TESS_EVALUATION_SHADER:
+		return "Tessellation Evaluation Shader";
+	case GL_COMPUTE_SHADER:
+		return "Compute Shader";
 	default:
 		return fmt::format("Unknown Shader Type ({0})", type).c_str();
 	}
